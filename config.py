12c08b81c33771f345adcad2c17373611429222e
seang12-cloud <157552293+seang12-cloud@users.noreply.github.com>
2024-01-24 15:27:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Replace the following with your API credentials, and rename this file to `config.py`
-OPENAI_API_KEY = "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"
-MJ_API_KEY = "1a98578957b2d67d4ae935f48f23d7272ae735a267cdf957254f633cfaadc3d8"
+OPENAI_API_KEY = ""
+MJ_API_KEY = ""
